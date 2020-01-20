29df5b7241d3ef82f4a0ffc649da6148d798c7db
cadtay <callum.taylor@carfinance247.co.uk>
2020-01-20 15:25:01 +0000

    Added diesel tax calculation

--- exercises/dotnet/TaxCalculator.Tests/TaxCalculatorDieselTest.cs
+++ exercises/dotnet/TaxCalculator.Tests/TaxCalculatorDieselTest.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 
 namespace TaxCalculator.Tests
 {
-    [Ignore("Not yet implemented")]
+
     class TaxCalculatorDieselTest
     {
         private static readonly DateTime FirstOfJanuary2019 = new DateTime(2019, 1, 1);
@@ -12,14 +12,14 @@ namespace TaxCalculator.Tests
         [SetUp]
         public void BeforeEach()
         {
-            _taxCalculator = new DummyTaxCalculator();
+            _taxCalculator = new DefaultTaxCalculator();
         }
 
         [Test]
         public void WhenVehicleHas0GramsCo2()
         {
             Vehicle vehicle = new Vehicle(0, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(0, tax);
         }
 
@@ -27,7 +27,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas50GramsCo2()
         {
             Vehicle vehicle = new Vehicle(50, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(25, tax);
         }
 
@@ -35,7 +35,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas75GramsCo2()
         {
             Vehicle vehicle = new Vehicle(75, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(105, tax);
         }
 
@@ -43,7 +43,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas90GramsCo2()
         {
             Vehicle vehicle = new Vehicle(90, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(125, tax);
         }
 
@@ -51,7 +51,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas100GramsCo2()
         {
             Vehicle vehicle = new Vehicle(100, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(145, tax);
         }
 
@@ -59,7 +59,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas110GramsCo2()
         {
             Vehicle vehicle = new Vehicle(110, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(165, tax);
         }
 
@@ -67,7 +67,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas130GramsCo2()
         {
             Vehicle vehicle = new Vehicle(130, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(205, tax);
         }
 
@@ -75,7 +75,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas150GramsCo2()
         {
             Vehicle vehicle = new Vehicle(150, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(515, tax);
         }
 
@@ -83,7 +83,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas170GramsCo2()
         {
             Vehicle vehicle = new Vehicle(170, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(830, tax);
         }
 
@@ -91,7 +91,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas190GramsCo2()
         {
             Vehicle vehicle = new Vehicle(190, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(1240, tax);
         }
 
@@ -99,7 +99,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas225GramsCo2()
         {
             Vehicle vehicle = new Vehicle(225, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(1760, tax);
         }
 
@@ -107,7 +107,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHas255GramsCo2()
         {
             Vehicle vehicle = new Vehicle(255, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(2070, tax);
         }
 
@@ -115,7 +115,7 @@ namespace TaxCalculator.Tests
         public void WhenVehicleHasOver255GramsCo2()
         {
             Vehicle vehicle = new Vehicle(256, FuelType.Diesel, FirstOfJanuary2019, 20000);
-            int tax = _taxCalculator.CalculateTax(vehicle);
+            int tax = _taxCalculator.TaxCal(vehicle);
             Assert.AreEqual(2070, tax);
         }
     }
--- exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
@@ -33,11 +33,36 @@ namespace TaxCalculator
                     dict.Add(225, 1240);
                     dict.Add(255, 1760);
 
-                         price = dict.Where(x => vehicle.Co2Emissions <= x.Key)
+                    price = dict.Where(x => vehicle.Co2Emissions <= x.Key)
                         .Select(xx => xx.Value).FirstOrDefault();
-
+                    
                     price =  vehicle.Co2Emissions > 255 ? 2070 : price;
                     break;
+
+                case FuelType.Diesel:
+
+                    dict.Add(0,0);
+                    dict.Add(50, 25);
+                    dict.Add(75, 105);
+                    dict.Add(90, 125);
+                    dict.Add(100, 145);
+                    dict.Add(110, 165);
+                    dict.Add(130, 205);
+                    dict.Add(150, 515);
+                    dict.Add(170, 830);
+                    dict.Add(190, 1240);
+                    dict.Add(225, 1760);
+                    dict.Add(255, 2070);
+
+                    price = dict.Where(x => vehicle.Co2Emissions <= x.Key).
+                        Select(xx => xx.Value).FirstOrDefault();
+
+                    price = vehicle.Co2Emissions > 255 ? 2070 : price;
+
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
             return price;
         }
