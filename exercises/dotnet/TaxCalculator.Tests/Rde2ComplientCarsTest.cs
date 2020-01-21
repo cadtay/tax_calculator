d06fb604c073cec2bbca30de4b95c9ea42ba0822
cadtay <callum.taylor@carfinance247.co.uk>
2020-01-21 15:06:35 +0000

    Rde2 complient cars

---
+++ exercises/dotnet/TaxCalculator.Tests/Rde2ComplientCarsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TaxCalculator.Tests
+{
+    class Rde2ComplientCarsTest
+    {
+        private static readonly DateTime FirstOfJanuary2019 = new DateTime(2019, 1, 1);
+        private TaxCalculator _taxCalculator;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _taxCalculator = new DefaultTaxCalculator(false);
+        }
+
+    }
+}
